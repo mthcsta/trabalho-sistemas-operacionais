fd06a8b49770a26e6d4e23ee946eb8325746ed01
Matheus Costa <matheuscostakrenn@gmail.com>
2023-05-12 11:34:58 -0300

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -15,28 +15,33 @@ public class Programa {
         Scanner teclado = new Scanner (System.in);
         AtomicBoolean emExecucao = new AtomicBoolean(true);
 
+        processos.popularProcessos();
+        processos.imprimeProcessos();
+
         while (emExecucao.get()) {
-            System.out.print("Escolha o argoritmo?: [1=FCFS 2=SJF Preemptivo 3=SJF Não Preemptivo  4=Prioridade Preemptivo 5=Prioridade Não Preemptivo  6=Round_Robin  7=Imprime lista de processos 8=Popular processos novamente 9=Sair]: ");
+            System.out.print("Escolha o argoritmo?: \n[1=FCFS 2=SJF Preemptivo 3=SJF Não Preemptivo  4=Prioridade Preemptivo 5=Prioridade Não Preemptivo  6=Round_Robin  7=Imprime lista de processos 8=Popular processos novamente 9=Sair]: \n");
             algoritmoUsado =  teclado.nextInt();
 
             switch (algoritmoUsado) {
                 case 1:
-                    FCFS::test(processos);
+                    System.out.println("Usando FCFS:");
+                    new FCFS(processos).teste();
+                    System.out.println("Finalizado!");
                     break;
                 case 2:
-                    SJF::test(true, processos);
+                    //SJF::test(true, processos);
                     break;
                 case 3:
-                    SJF::test(false, processos);
+                    new SJF(processos).naoPreemptivo();
                     break;
                 case 4:
-                    PRIORIDADE::test(true, processos);
+                    //PRIORIDADE::test(true, processos);
                     break;
                 case 5:
-                    PRIORIDADE::test(false, processos);
+                    //PRIORIDADE::test(false, processos);
                     break;
                 case 6:
-                    RoundRobin::test(processos);
+                    //RoundRobin::test(processos);
                     break;
                 case 7:
                     processos.imprimeProcessos();
