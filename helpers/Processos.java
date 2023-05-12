89639a42a665bdd15d7b00ba018f662734056e22
Matheus Costa <matheuscostakrenn@gmail.com>
2023-05-12 11:34:17 -0300

    Update Processos.java

--- helpers/Processos.java
+++ helpers/Processos.java
@@ -3,7 +3,7 @@ package helpers;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Processos {
+public class Processos implements Cloneable {
     Processo[] processos;
 
     public Processos(int numeroDeProcessos) {
@@ -21,38 +21,73 @@ public class Processos {
         Scanner teclado = new Scanner (System.in);
 
         System.out.print("Será aleatório?:  ");
-        aleatorio = teclado.nextInt();
+        aleatorio = 2; //teclado.nextInt();
 
-        for (int i = 0; i < this.processos.length; i++) {
-            //Popular helpers.Processos Aleatorio
-            if (aleatorio == 1){
-                tempoExecucao = random.nextInt(10)+1;
-                tempoChegada = random.nextInt(10)+1;
-                prioridade = random.nextInt(10)+1;
-            }
-            //Popular helpers.Processos Manual
-            else {
-                System.out.print("Digite o tempo de execução do processo["+i+"]:  ");
-                tempoExecucao = teclado.nextInt();
-                System.out.print("Digite o tempo de chegada do processo["+i+"]:  ");
-                tempoChegada = teclado.nextInt();
-                System.out.print("Digite a prioridade do processo["+i+"]:  ");
-                prioridade = teclado.nextInt();
-            }
-            tempoRestante = tempoExecucao;
+        if (aleatorio == 2) {
+            this.processos[0] = new Processo(10, 3, 9, 10);
+            this.processos[1] = new Processo(7, 7, 7, 7);
+            this.processos[2] = new Processo(3, 2, 15, 3);
+        } else {
+            for (int i = 0; i < this.processos.length; i++) {
+                //Popular helpers.Processos Aleatorio
+                if (aleatorio == 1){
+                    tempoExecucao = random.nextInt(10)+1;
+                    tempoChegada = random.nextInt(10)+1;
+                    prioridade = random.nextInt(10)+1;
+                }
+                //Popular helpers.Processos Manual
+                else {
+                    System.out.print("Digite o tempo de execução do processo["+i+"]:  ");
+                    tempoExecucao = teclado.nextInt();
+                    System.out.print("Digite o tempo de chegada do processo["+i+"]:  ");
+                    tempoChegada = teclado.nextInt();
+                    System.out.print("Digite a prioridade do processo["+i+"]:  ");
+                    prioridade = teclado.nextInt();
+                }
+                tempoRestante = tempoExecucao;
 
-            this.processos[i].setTempoExecucao(tempoExecucao)
-                    .setTempoChegada(tempoChegada)
-                    .setPrioridade(prioridade)
-                    .setTempoRestante(tempoRestante);
+                this.processos[i] = new Processo(tempoExecucao, tempoChegada, prioridade, tempoRestante);
+            }
         }
+
     }
 
     public void imprimeProcessos()
     {
         //Imprime lista de processos
         for (int indice = 0; indice < this.processos.length; indice++) {
-            System.out.printf("helpers.Processo[\"%d\"]: %s", indice, this.getProcessoPorIndice(indice));
+            System.out.printf("Processo[\"%d\"]: %s\n", indice, this.getProcessoPorIndice(indice));
         }
+    }
+
+    public Processo[] getProcessos() {
+        return processos;
+    }
+
+    public Processos getClone() {
+        try {
+            return (Processos) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean hasProcessoIndice(int processoIndice) {
+        return processoIndice > 0 && processoIndice < this.processos.length;
+    }
+
+    public void setProcesso(int processoIndice, Processo processo) {
+        this.processos[processoIndice] = processo;
+    }
+
+    public void imprimeEstatisticas() {
+        float tempoMedio = 0;
+        for (int indice = 0; indice < processos.length; indice++) {
+            int tempoEspera = getProcessoPorIndice(indice).getTempoEspera();
+            tempoMedio += tempoEspera;
+            System.out.printf("Processo[%d]: tempo_espera=%d\n", indice, tempoEspera);
+        }
+        tempoMedio /= processos.length;
+        System.out.printf("Tempo médio de espera: %f", tempoMedio);
     }
 }
