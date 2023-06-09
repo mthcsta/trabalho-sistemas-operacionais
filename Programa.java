e686440e0af357a6410b127cb9fe512ccd3eddaf
Matheus Costa <matheuscostakrenn@gmail.com>
2023-06-09 16:30:10 -0300

    Melhoria na leitura do menu de opcoes

--- Programa.java
+++ Programa.java
@@ -14,21 +14,31 @@ public class Programa {
         int algoritmoUsado;
         int numeroDeProcessos = 3;
         Processos processos = new Processos(numeroDeProcessos);
-        Scanner teclado = new Scanner (System.in);
+        Scanner teclado = new Scanner(System.in);
         AtomicBoolean emExecucao = new AtomicBoolean(true);
 
         processos.popularProcessos();
         processos.imprimeProcessos();
 
         while (emExecucao.get()) {
-            System.out.print("Escolha o argoritmo?: \n[1=FCFS 2=SJF Preemptivo 3=SJF Não Preemptivo  4=Prioridade Preemptivo 5=Prioridade Não Preemptivo  6=Round_Robin  7=Imprime lista de processos 8=Popular processos novamente 9=Sair]: \n");
+            System.out.print(
+                    "MENU DE OPÇÕES\n" +
+                    "1=FCFS\n" +
+                    "2=SJF Preemptivo\n" +
+                    "3=SJF Não Preemptivo\n" +
+                    "4=Prioridade Preemptivo\n" +
+                    "5=Prioridade Não Preemptivo\n" +
+                    "6=Round_Robin\n" +
+                    "7=Imprime lista de processos\n" +
+                    "8=Popular processos novamente\n" +
+                    "9=Sair]\n" +
+                    "Escolha uma opção: "
+            );
             algoritmoUsado =  teclado.nextInt();
 
             switch (algoritmoUsado) {
                 case 1:
-                    System.out.println("Usando FCFS:");
                     new FCFS(processos).teste();
-                    System.out.println("Finalizado!");
                     break;
                 case 2:
                     new SJFPreemptivo(processos).teste();
